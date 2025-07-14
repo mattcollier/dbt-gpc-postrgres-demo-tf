b0866416379cc529eb739b3bc272b75069a97dcd
Matthew Collier <collier.matthew@gmail.com>
2025-07-14 13:42:44 -0400

    Add core files.

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = "${var.region}-b"
+}
+
+resource "google_compute_firewall" "allow_postgres" {
+  name    = "allow-postgres-dbt"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+  source_ranges = var.dbt_cloud_cidrs   # limit exposure to dbt Cloud
+}
+
+resource "google_compute_instance" "pg" {
+  name         = "pg-demo"
+  machine_type = "e2-micro"            # free-tier size  [oai_citation:4‡Google Cloud](https://cloud.google.com/free/docs/compute-getting-started?utm_source=chatgpt.com)
+  zone         = "${var.region}-b"
+  tags         = ["postgres"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-12"
+      size  = 20                       # still within 30 GB free allotment
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}                  # ephemeral external IP
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/usr/bin/env bash
+    apt-get update -y && apt-get install -y docker.io curl postgresql-client
+    systemctl enable --now docker
+
+    # Launch Postgres in a container
+    docker run -d --name pg \
+      -e POSTGRES_USER=${var.db_user} \
+      -e POSTGRES_PASSWORD=${var.db_password} \
+      -e POSTGRES_DB=${var.db_name} \
+      -p 5432:5432 \
+      postgres:16
+
+    # Wait until Postgres responds
+    until docker exec pg pg_isready -U ${var.db_user}; do sleep 2; done
+
+    # Load the Jaffle Shop sample data
+    curl -sL https://raw.githubusercontent.com/dbt-labs/jaffle-shop-data/main/postgres/load_postgres.sql \
+      -o /tmp/load.sql
+    PGPASSWORD=${var.db_password} psql -h localhost -U ${var.db_user} -d ${var.db_name} -f /tmp/load.sql
+  EOF
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_id"            {}
+variable "region"     { default = "us-central1" }    # free-tier region
+variable "db_user"    { default = "dbt" }
+variable "db_password"           {}
+variable "db_name"    { default = "jaffleshop" }
+
+# dbt Cloud publishes its egress ranges here:
+# https://docs.getdbt.com/docs/cloud/about-cloud/access-regions-ip-addresses
+variable "dbt_cloud_cidrs" {
+  type    = list(string)
+  default = [
+    "52.45.144.63/32",
+    "54.81.134.249/32",
+    "52.22.161.231/32",
+    "52.3.77.232/32",
+    "3.214.191.130/32",
+    "34.233.79.135/32"
+  ]                                             # North-America (us-east-1) cell  [oai_citation:3‡dbt Developer Hub](https://docs.getdbt.com/docs/cloud/about-cloud/access-regions-ip-addresses)
+}
